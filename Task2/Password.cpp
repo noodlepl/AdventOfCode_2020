2de408d27f5178a0c662ce54a46006a929a76510
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-06 20:45:21 +0100

    add Password class

---
+++ Task2/Password.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Kepinski on 06/12/2020.
+//
+
+#include "Password.h"
---
+++ Task2/Password.h
@@ -0,0 +1,14 @@
+//
+// Created by Adam Kepinski on 06/12/2020.
+//
+
+#ifndef ADVENTOFCODE_2020_PASSWORD_H
+#define ADVENTOFCODE_2020_PASSWORD_H
+
+
+class Password {
+
+};
+
+
+#endif //ADVENTOFCODE_2020_PASSWORD_H
