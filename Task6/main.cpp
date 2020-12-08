9ed6d6d204b91585a3e8571d64b59d7e410d1abe
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-08 17:54:06 +0100

    task6 part 1 complete

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -6,26 +6,38 @@
 #include <sstream>
 #include <set>
 
-void readData(){
+void handleGroup(std::set<char>& group, std::vector<int>& yes_counts) {
+    std::cout << "Group answers: ";
+    std::for_each(group.begin(), group.end(), [](auto a){std::cout << a;});
+    std::cout << "\n";
+    yes_counts.push_back(group.size());
+    group.clear();
+}
+
+void countAnswers(){
     std::ifstream ifs("input.txt", std::ios_base::in);
     std::string line;
     std::set<char> group_answers;
     std::vector<int> yes_counts;
-    std::string group_delimiter = "\n";
 
     while (std::getline(ifs, line)){
-        std::copy(line.begin(), line.end(), std::back_inserter(group_answers));
+        std::copy(line.begin(), line.end(), std::inserter(group_answers, group_answers.begin()));
 
-        if (line == group_delimiter) {
-            yes_counts.push_back(group_answers.size());
-            group_answers.clear();
+        if (line.empty()) {
+            handleGroup(group_answers, yes_counts);
         }
     }
+    handleGroup(group_answers, yes_counts);
+
+    int sum = 0;
+    std::for_each(yes_counts.begin(), yes_counts.end(), [&sum](auto i){sum += i;});
+    std::cout << "Unique \"yeses\": " << sum << std::endl;
 }
 
 int main() {
     auto start = std::chrono::system_clock::now();
 
+    countAnswers();
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
