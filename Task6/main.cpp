8a0d5af4fe30bd8271a71b71cf11d5f48c32cbeb
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-08 18:19:34 +0100

    task6 part 2 complete

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <set>
 
-void handleGroup(std::set<char>& group, std::vector<int>& yes_counts) {
+void handleGroupPart1(std::set<char>& group, std::vector<int>& yes_counts) {
     std::cout << "Group answers: ";
     std::for_each(group.begin(), group.end(), [](auto a){std::cout << a;});
     std::cout << "\n";
@@ -14,7 +14,7 @@ void handleGroup(std::set<char>& group, std::vector<int>& yes_counts) {
     group.clear();
 }
 
-void countAnswers(){
+void countAnswersPart1(){
     std::ifstream ifs("input.txt", std::ios_base::in);
     std::string line;
     std::set<char> group_answers;
@@ -24,20 +24,57 @@ void countAnswers(){
         std::copy(line.begin(), line.end(), std::inserter(group_answers, group_answers.begin()));
 
         if (line.empty()) {
-            handleGroup(group_answers, yes_counts);
+            handleGroupPart1(group_answers, yes_counts);
         }
     }
-    handleGroup(group_answers, yes_counts);
+    handleGroupPart1(group_answers, yes_counts);
 
     int sum = 0;
     std::for_each(yes_counts.begin(), yes_counts.end(), [&sum](auto i){sum += i;});
     std::cout << "Unique \"yeses\": " << sum << std::endl;
 }
 
+void handleGroupPart2(std::vector<std::string>& group_answers, std::vector<int>& yeses_count) {
+    std::set<char> common_answers;
+    std::for_each(group_answers.begin(), group_answers.end(), [&common_answers, &group_answers](auto answers) {
+        std::copy_if(answers.begin(), answers.end(), std::inserter(common_answers, common_answers.begin()), [&group_answers](auto a){
+            return std::all_of(group_answers.begin(), group_answers.end(), [a](auto answer){
+                return answer.find(a) != std::string::npos;
+            });
+        });
+    });
+    std::cout << "Everyone answered: ";
+    std::for_each(common_answers.begin(), common_answers.end(), [](auto a){std::cout << a;});
+    std::cout << "\n";
+    yeses_count.push_back(common_answers.size());
+
+    group_answers.clear();
+}
+
+void countAnswersPart2() {
+    std::ifstream ifs("input.txt", std::ios_base::in);
+    std::string line;
+    std::vector<std::string> group_answers;
+    std::vector<int> yeses_count;
+
+    while (std::getline(ifs, line)) {
+        if (line.empty()) {
+            handleGroupPart2(group_answers, yeses_count);
+            continue;
+        }
+        group_answers.push_back(line);
+    }
+    handleGroupPart2(group_answers, yeses_count);
+
+    int sum = 0;
+    std::for_each(yeses_count.begin(), yeses_count.end(), [&sum](auto i){sum += i;});
+    std::cout << "Unique \"yeses\": " << sum << std::endl;
+}
+
 int main() {
     auto start = std::chrono::system_clock::now();
 
-    countAnswers();
+    countAnswersPart2();
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
