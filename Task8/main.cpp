043a6cfc8f47954de18630a8bb07ad4f9f53bcb3
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-10 00:14:41 +0100

    task 8 part 2

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -56,21 +56,53 @@ int execute(const Instructions& instructions, int instr_index, int& accumulated_
     throw std::invalid_argument("unknown instruction");
 }
 
+void switchType(std::string& type) {
+    if (type == "nop") type = "jmp";
+    if (type == "jmp") type = "nop";
+}
+
 int main() {
     auto start = std::chrono::system_clock::now();
 
-    auto instructions = readData();
+    auto original_instructions = readData();
 
-    std::vector<int> executed_indexes;
-    int accumulated_value = 0;
-    int current_index = 0;
+    //Part 1
+//    std::vector<int> executed_indexes;
+//    int accumulated_value = 0;
+//    int current_index = 0;
+//
+//    do {
+//        executed_indexes.push_back(current_index);
+//        current_index = execute(original_instructions, current_index, accumulated_value);
+//    } while (std::find(executed_indexes.begin(), executed_indexes.end(), current_index) == executed_indexes.end());
+//
+//    std::cout << "Accumulated value in part 1: " << accumulated_value << std::endl;
 
-    do {
-        executed_indexes.push_back(current_index);
-        current_index = execute(instructions, current_index, accumulated_value);
-    } while (std::find(executed_indexes.begin(), executed_indexes.end(), current_index) == executed_indexes.end());
+    //Part 2
+    for (int i = 0; i < original_instructions.size(); ++i) {
+        Instruction& instr = original_instructions[i];
+        if (instr.type == "acc") continue;
+        Instructions fixed_instructions(original_instructions);
+        switchType(fixed_instructions[i].type);
 
-    std::cout << "Accumulated value: " << accumulated_value << std::endl;
+        std::vector<int> executed_indexes;
+        int accumulated_value = 0;
+        int current_index = 0;
+        bool fixed = false;
+
+        do {
+            executed_indexes.push_back(current_index);
+            current_index = execute(fixed_instructions, current_index, accumulated_value);
+            if (current_index >= fixed_instructions.size()) {
+                fixed = true;
+                break;
+            }
+        } while (std::find(executed_indexes.begin(), executed_indexes.end(), current_index) == executed_indexes.end());
+        if (fixed) {
+            std::cout << "Accumulated value in part 2: " << accumulated_value << std::endl;
+            break;
+        }
+    }
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
