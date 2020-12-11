9d8eb8781cc8e77e7bfeda7bb955b8b61ec16f76
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-11 10:48:35 +0100

    Task11 test code works

--- Task11/Matrix.h
+++ Task11/Matrix.h
@@ -6,30 +6,86 @@
 #define ADVENTOFCODE_2020_MATRIX_H
 #include <algorithm>
 #include <iostream>
+#include <istream>
 
 template<typename T>
 class Matrix {
     std::vector<T> data_;
     int columns_;
+    int rows_;
 
-public:
-    Matrix(int columns) : columns_(columns) {
+    std::pair<int, int> posToRowColumn(int pos){
+        return std::make_pair(pos / columns_, pos % columns_);
     }
 
-    void addRow(const std::vector<T> row) {
-        assert(row.size() == columns_);
-        std::copy(row.begin(), row.end(), std::back_inserter(data_));
+public:
+    explicit Matrix(std::istream& data_stream) {
+        std::string row;
+        rows_ = 0;
+        while (std::getline(data_stream, row)) {
+            std::copy(row.begin(), row.end(), std::back_inserter(data_));
+            ++rows_;
+        }
+        columns_ = row.size();
+    }
+
+    T get(int row, int column) {
+        assert(column < columns_);
+        assert(row < rows_);
+        return data_[row * columns_ + column];
+    }
+
+    void set(int row, int column, T value) {
+        assert(column < columns_);
+        assert(row < rows_);
+        data_[row * columns_ + column] = value;
+    }
+
+    std::vector<T> getSurrounding(int row, int column) {
+        std::vector<T> surrounding;
+        for (int r = row - 1; r <= row+1; ++r) {
+            if (r < 0) continue;
+            if (r >= rows_) break;
+            for (int c = column - 1; c <= column+1; ++c) {
+                if (c < 0 || (r == row && c == column)) continue;
+                if (c >= columns_) break;
+                surrounding.push_back(get(r, c));
+            }
+        }
+        return surrounding;
+    }
+
+    auto insert(typename std::vector<T>::iterator it, T value) {
+        return data_.insert(it, value);
+    }
+
+    std::vector<T> getSurrounding(typename std::vector<T>::iterator it) {
+        auto pos = it - data_.begin();
+        auto [row, column] = posToRowColumn(pos);
+        return getSurrounding(row, column);
+    }
+
+    typename std::vector<T>::iterator begin() {
+        return data_.begin();
+    }
+
+    typename std::vector<T>::iterator end() {
+        return data_.end();
     }
 
     bool operator==(const Matrix<T>& other) {
-        return columns_ == other.columns_ && data_ == other.data_;
+        return columns_ == other.columns_ && rows_ == other.rows_ && data_ == other.data_;
+    }
+
+    bool operator!=(const Matrix<T>& other) {
+        return !(*this == other);
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
-        for (int pos = 0; pos < data_.size(); ++pos) {
-            os << data_[pos];
-            if (pos % columns_ == 0)
-                os << "\n"
+        for (int pos = 0; pos < matrix.data_.size(); ++pos) {
+            os << matrix.data_[pos];
+            if (pos % matrix.columns_ == matrix.columns_ - 1)
+                os << "\n";
         }
 
         return os;
--- Task11/main.cpp
+++ Task11/main.cpp
@@ -5,10 +5,55 @@
 #include <chrono>
 #include <sstream>
 #include <cstdint>
+#include "Matrix.h"
+
+Matrix<char> readData() {
+    std::ifstream ifs("input.txt");
+    Matrix<char> matrix(ifs);
+
+    return matrix;
+}
 
 int main() {
     auto start = std::chrono::system_clock::now();
 
+    auto next_matrix = readData();
+    auto original_matrix = next_matrix;
+    std::cout << next_matrix << "\n";
+
+    do {
+        original_matrix = next_matrix;
+        auto original_it = original_matrix.begin();
+        auto next_it = next_matrix.begin();
+        for(; original_it != original_matrix.end(); ++original_it, ++next_it) {
+            switch(*original_it) {
+                case '.':
+                    break;
+                case 'L':
+                {
+                    auto surrounding = original_matrix.getSurrounding(original_it);
+                    auto occupied_it = std::find(surrounding.begin(), surrounding.end(), '#');
+                    if (occupied_it == surrounding.end()) {
+                        *next_it = '#';
+                    }
+                    break;
+                }
+                case '#':
+                {
+                    auto surrounding = original_matrix.getSurrounding(original_it);
+                    auto occupied_count = std::count(surrounding.begin(), surrounding.end(), '#');
+                    if (occupied_count >= 4) {
+                        *next_it = 'L';
+                    }
+                    break;
+                }
+            }
+        }
+        std::cout << next_matrix << "\n";
+    } while (next_matrix != original_matrix);
+
+    auto occupied_seats = std::count(original_matrix.begin(), original_matrix.end(), '#');
+    std::cout << "There are: " << occupied_seats << " occupied seats.\n";
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
