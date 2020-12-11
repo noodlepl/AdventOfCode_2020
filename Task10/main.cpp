234f992fd09dd0ffca5bb6f933e08793fa34861e
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-11 07:51:07 +0100

    Task 10 part 2 complete

--- Task10/main.cpp
+++ Task10/main.cpp
@@ -17,6 +17,25 @@ std::vector<int> readData() {
     return data;
 }
 
+bool transitionValid(const std::vector<int>& data, int current_id, int prev_id) {
+    return (prev_id >= 0) && (data[current_id] - data[prev_id] <= 3);
+}
+
+template <typename Num>
+Num countSequences(const std::vector<int>& data) {
+    std::vector<Num> sequence_counts{1};
+
+    for (int i = 1; i < data.size(); ++i) {
+        int64_t ends_with_i_count = sequence_counts[i-1];
+        if (transitionValid(data, i, i-2)) ends_with_i_count += sequence_counts[i-2];
+        if (transitionValid(data, i, i-3)) ends_with_i_count += sequence_counts[i-3];
+
+        sequence_counts.push_back(ends_with_i_count);
+    }
+
+    return sequence_counts.back();
+}
+
 int main() {
     auto start = std::chrono::system_clock::now();
 
@@ -31,6 +50,7 @@ int main() {
     std::cout << std::endl;
     std::cout << "Length: " << data.size() << "\n";
 
+    // Part 1
     auto prev = data.begin();
     auto ones_count = 0;
     auto threes_count = 0;
@@ -44,6 +64,10 @@ int main() {
     }
 
     std::cout << "1 jolts diffs: " << ones_count << ", 3 jolts diffs: " << threes_count << ", multiplication: " << ones_count * threes_count << "\n";
+
+    // Part 2
+    auto valid_sequences_count = countSequences<int64_t>(data);
+    std::cout << "Valid sequence count: " << valid_sequences_count << std::endl;
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
