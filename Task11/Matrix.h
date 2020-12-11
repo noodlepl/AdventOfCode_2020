ed4e6a0296ff9c8930dfc0651888f273c8c46f8c
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-11 08:38:35 +0100

    Task 11 matrix

--- Task11/Matrix.h
+++ Task11/Matrix.h
@@ -5,21 +5,34 @@
 #ifndef ADVENTOFCODE_2020_MATRIX_H
 #define ADVENTOFCODE_2020_MATRIX_H
 #include <algorithm>
+#include <iostream>
 
 template<typename T>
 class Matrix {
     std::vector<T> data_;
-    int rows_;
     int columns_;
 
 public:
-    Matrix(int rows, int columns) : rows_(rows), columns_(columns) {
-        data_.reserve(rows * columns);
+    Matrix(int columns) : columns_(columns) {
     }
 
     void addRow(const std::vector<T> row) {
-        assert(row.size() == rows_);
+        assert(row.size() == columns_);
         std::copy(row.begin(), row.end(), std::back_inserter(data_));
+    }
+
+    bool operator==(const Matrix<T>& other) {
+        return columns_ == other.columns_ && data_ == other.data_;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+        for (int pos = 0; pos < data_.size(); ++pos) {
+            os << data_[pos];
+            if (pos % columns_ == 0)
+                os << "\n"
+        }
+
+        return os;
     }
 };
 
