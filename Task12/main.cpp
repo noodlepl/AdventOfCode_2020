375d14608d4a61a09e122f9db55215a8792bf1a7
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-12 11:11:48 +0100

    Task 12 init

---
+++ Task12/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <sstream>
+#include <cstdint>
+
+void readData() {
+
+}
+
+int main() {
+    auto start = std::chrono::system_clock::now();
+
+    auto end = std::chrono::system_clock::now();
+    std::chrono::microseconds diff = end - start;
+    std::cout << "Program duration: " << diff.count() << " microseconds" << std::endl;
+    return 0;
+}
