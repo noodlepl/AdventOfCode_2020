16da19d00fb27e7a7b7a6a6fbb71dfa589b71d94
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-14 18:02:17 +0100

    Task 14 part 1 complete

--- Task14/main.cpp
+++ Task14/main.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <cstdint>
 #include <map>
+#include <regex>
 
 using bitval = std::bitset<36>;
 bitval g_mask_or;
@@ -14,15 +15,19 @@ bitval g_mask_and = ~g_mask_or;
 std::map<int64_t, bitval> address_space_;
 
 void updateMasks(const std::string& mask) {
+    std::string delim = "= ";
+    auto mask_bits_pos = mask.find(delim);
+    auto mask_bits = mask.substr(mask_bits_pos + delim.size());
+
     std::string mask_or;
-    std::transform(mask.begin(), mask.end(), std::back_inserter(mask_or), [](auto a){
+    std::transform(mask_bits.begin(), mask_bits.end(), std::back_inserter(mask_or), [](auto a){
         if (a == 'X') a = '0';
         return a;
     });
     g_mask_or = bitval(mask_or);
 
     std::string mask_and;
-    std::transform(mask.begin(), mask.end(), std::back_inserter(mask_and), [](auto a){
+    std::transform(mask_bits.begin(), mask_bits.end(), std::back_inserter(mask_and), [](auto a){
         if (a == 'X') a = '1';
         return a;
     });
@@ -30,19 +35,27 @@ void updateMasks(const std::string& mask) {
 }
 
 void assignValue(const std::string& instruction) {
-    bitval bit_instr(instruction);
+    std::regex r("^mem\\[([0-9]+)\\] = ([0-9]+)$");
+    std::smatch match;
+    std::regex_search(instruction, match, r);
+    auto key = std::stoull(match[1]);
+    auto value = std::stoull(match[2]);
+    bitval value_bits(value);
+    value_bits &= g_mask_and;
+    value_bits |= g_mask_or;
+//    std::cout << "Inserting: " << value_bits << "\n";
+    address_space_[key] = value_bits;
 }
 
 void executeLine(const std::string& line) {
-
+    if (line.find("mask") != std::string::npos)
+        updateMasks(line);
+    else if(line.find("mem") != std::string::npos)
+        assignValue(line);
 }
 
 int main() {
     auto start = std::chrono::system_clock::now();
-
-    std::string input_mask("0110010011X1X1X0000000XX0110000XX111");
-    updateMasks(input_mask);
-    std::cout << "input mask:\t" << input_mask << "\nmask OR:\t" << g_mask_or << "\nmask AND:\t" << g_mask_and << "\n";
 
     std::ifstream ifs("input.txt", std::ios_base::in);
     std::string line;
@@ -50,6 +63,12 @@ int main() {
         executeLine(line);
     }
 
+    auto sum = std::accumulate(address_space_.begin(), address_space_.end(), 0ULL, [](auto sum, auto&& element) {
+        auto value = element.second.to_ullong();
+        return sum + value;
+    });
+
+    std::cout << "Result: " << sum << "\n";
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
     std::cout << "Program duration: " << diff.count() << " microseconds" << std::endl;
