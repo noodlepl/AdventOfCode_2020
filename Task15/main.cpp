12d8b0a2ebac4d0346075c7f119ee9067524e273
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-15 18:33:10 +0100

    Task 15 init

---
+++ Task15/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <sstream>
+#include <cstdint>
+#include <map>
+#include <regex>
+
+int main() {
+    auto start = std::chrono::system_clock::now();
+
+    auto end = std::chrono::system_clock::now();
+    std::chrono::microseconds diff = end - start;
+    std::cout << "Program duration: " << diff.count() << " microseconds" << std::endl;
+    return 0;
+}
