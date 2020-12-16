4903ea44c7fff855ecac23912631ec0787697dcb
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-16 17:37:47 +0100

    Task16 data input

--- Task16/main.cpp
+++ Task16/main.cpp
@@ -18,19 +18,42 @@ Ticket g_my_ticket;
 std::vector<Ticket> g_other_tickets;
 
 void addRule(std::string line) {
-
+    std::regex r("^(.*): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)$");
+    std::smatch results;
+    std::regex_search(line, results, r);
+    assert(results.size() == 6);
+    auto key = results[1];
+    auto first_range = std::make_pair(std::stoi(results[2]), std::stoi(results[3]));
+    auto second_range = std::make_pair(std::stoi(results[4]), std::stoi(results[5]));
+    auto [it, result] = g_rules.insert({key, std::make_pair(first_range, second_range)});
+    assert(result);
 }
 
-Ticket readTicket(std::string) {
-
+Ticket readTicket(std::string line) {
+    std::istringstream iss(line);
+    std::string token;
+    auto delimiter = ',';
+    Ticket ticket;
+    while(std::getline(iss, token, delimiter))
+        ticket.push_back(std::stoi(token));
+    return ticket;
 }
 
 void readData() {
-    std::ifstream ifs("intput.txt");
+    std::ifstream ifs("input.txt");
     std::string line;
     int state = 0;
     while(std::getline(ifs, line)) {
-        
+        std::cout << line << "\n";
+        if (line.empty()) continue;
+        else if(line == "your ticket:") {
+            state = 1;
+            continue;
+        }
+        else if(line == "nearby tickets:") {
+            state = 2;
+            continue;
+        }
 
         switch (state) {
             case 0:
@@ -50,6 +73,7 @@ void readData() {
 int main() {
     auto start = std::chrono::system_clock::now();
 
+    readData();
 
     auto end = std::chrono::system_clock::now();
     std::chrono::microseconds diff = end - start;
