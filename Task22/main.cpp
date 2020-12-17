6ef98735879ae31af33982ceb44451bd213b0938
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-17 17:51:46 +0100

    Remaining tasks templates

---
+++ Task22/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <sstream>
+#include <cstdint>
+#include <map>
+#include <regex>
+#include <array>
+#include <list>
+
+int main() {
+    auto start = std::chrono::system_clock::now();
+    
+    auto end = std::chrono::system_clock::now();
+    std::chrono::microseconds diff = end - start;
+    std::cout << "Program duration: " << diff.count() << " microseconds" << std::endl;
+    return 0;
+}
