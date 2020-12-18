27ed433258f5a345ae1318c9edc890a50611239b
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-18 15:49:08 +0100

    Task17 part 2 complete

--- Task17/main.cpp
+++ Task17/main.cpp
@@ -18,8 +18,9 @@ struct Coordinate {
     int x;
     int y;
     int z;
+    int w;
     friend bool operator==(const Coordinate& lhs, const Coordinate& rhs) {
-        return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
+        return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
     }
 };
 
@@ -30,7 +31,8 @@ struct CoordHash
         std::size_t h1 = std::hash<int>{}(coordinate.x);
         std::size_t h2 = std::hash<int>{}(coordinate.y);
         std::size_t h3 = std::hash<int>{}(coordinate.z);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
+        std::size_t h4 = std::hash<int>{}(coordinate.w);
+        return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3);
     }
 };
 
@@ -54,7 +56,7 @@ std::pair<Cubes, int> readData() {
     for (int row = start_value; row < end_value; ++row) {
         for (int col =  start_value; col < end_value; ++col) {
             if(*it == '#') {
-                auto [iterator, success] = active_cubes.insert(Coordinate{col, row, 0});
+                auto [iterator, success] = active_cubes.insert(Coordinate{col, row, 0, 0});
                 assert(success);
             }
             ++it;
@@ -65,32 +67,37 @@ std::pair<Cubes, int> readData() {
 }
 
 void printCubes(const Cubes& cubes, const Coordinate& start_coord) {
-    auto [start_x, start_y, start_z] = start_coord;
+    auto [start_x, start_y, start_z, start_w] = start_coord;
     assert(start_x <= 0 && start_y <= 0 && start_z <= 0);
-    for (int plane = start_z; plane <= -start_z; ++plane) {
-        for (int row = start_y; row <= -start_y; ++row) {
-            for (int col = start_x; col <= -start_x; ++col) {
-                if (std::find(cubes.begin(), cubes.end(), Coordinate{col, row, plane}) != cubes.end()) {
-                    std::cout << '#';
-                } else {
-                    std::cout << '.';
+    for (int hyper = start_w; hyper <= -start_w; ++hyper) {
+        for (int plane = start_z; plane <= -start_z; ++plane) {
+            std::cout << "z=" << plane << ", w=" << hyper <<"\n";
+            for (int row = start_y; row <= -start_y; ++row) {
+                for (int col = start_x; col <= -start_x; ++col) {
+                    if (std::find(cubes.begin(), cubes.end(), Coordinate{col, row, plane, hyper}) != cubes.end()) {
+                        std::cout << '#';
+                    } else {
+                        std::cout << '.';
+                    }
                 }
+                std::cout << "\n";
             }
             std::cout << "\n";
         }
-        std::cout << "\n";
     }
 }
 
 bool activate(const Cubes& cubes, const Coordinate& coord) {
-    auto [col,row,plane] = coord;
+    auto [col,row,plane,hyper] = coord;
     auto active_count = 0;
     for (auto x = col-1; x <= col+1; ++x){
         for (auto y = row-1; y <= row+1; ++y){
             for (auto z = plane-1; z <= plane+1; ++z){
-                if (x == col && y == row && z == plane) continue;
-                if (cubes.find(Coordinate{x, y, z}) != cubes.end())
-                    ++active_count;
+                for (auto w = hyper-1; w <=hyper+1; ++w) {
+                    if (x == col && y == row && z == plane && w == hyper) continue;
+                    if (cubes.find(Coordinate{x, y, z, w}) != cubes.end())
+                        ++active_count;
+                }
             }
         }
     }
@@ -105,14 +112,15 @@ bool activate(const Cubes& cubes, const Coordinate& coord) {
 
 Cubes runCycle(Cubes current, const Coordinate& start_coord) {
     Cubes next;
-    auto [start_x, start_y, start_z] = start_coord;
-    assert(start_x <= 0 && start_y <= 0 && start_z <= 0);
+    auto [start_x, start_y, start_z, start_w] = start_coord;
+    assert(start_x <= 0 && start_y <= 0 && start_z <= 0 && start_w <=0);
 
     for (auto x = start_x; x <= -start_x; ++x) {
         for (auto y = start_y; y <= -start_y; ++y) {
             for (auto z = start_z; z <= -start_z; ++z) {
-                if (activate(current, Coordinate{x,y,z}))
-                    next.insert(Coordinate{x,y,z});
+                for (auto w = start_w; w <= -start_w; ++w)
+                    if (activate(current, Coordinate{x,y,z,w}))
+                        next.insert(Coordinate{x,y,z,w});
             }
         }
     }
@@ -125,11 +133,11 @@ int main() {
 
     auto [cubes, dimension] = readData();
 
-//    printCubes(cubes, Coordinate{-2, -2, -1});
+//    printCubes(cubes, Coordinate{-4, -4, 0, 0});
 
-    Coordinate start_coord{-dimension/2, -dimension/2, 0};
+    Coordinate start_coord{-dimension/2, -dimension/2, 0, 0};
     for (int i = 0; i < 6; ++i){
-        start_coord = {--start_coord.x, --start_coord.y, --start_coord.z};
+        start_coord = {--start_coord.x, --start_coord.y, --start_coord.z, --start_coord.w};
         cubes = runCycle(cubes, start_coord);
     }
 
