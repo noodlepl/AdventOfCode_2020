b312ede5ef3248adae011e4514252080046bcb85
Adam Kępiński <adam.kepinski@tomtom.com>
2020-12-19 13:42:06 +0100

    Task18 part 2 complete

--- Task18/main.cpp
+++ Task18/main.cpp
@@ -43,28 +43,40 @@ void handleBrackets(std::string& line) {
     }
 }
 
+void handleAdditions(std::string& line) {
+    for (auto operator_pos = line.find(kPlus); operator_pos != std::string::npos; operator_pos = line.find(kPlus, operator_pos)) {
+        auto operand1_pos = line.rfind(kTimes, operator_pos-1);
+        if (operand1_pos == std::string::npos)
+            operand1_pos = 0;
+        else
+            ++operand1_pos;
+
+        auto operand1 = std::stoll(line.substr(operand1_pos, operator_pos));
+        auto next_op_pos = line.find_first_of("*+", operator_pos + 1);
+        auto operand2 = std::stoll(line.substr(operator_pos+1, next_op_pos));
+        int64_t result = operand1 + operand2;
+        line.erase(operand1_pos, next_op_pos - operand1_pos);
+        line.insert(operand1_pos, std::to_string(result));
+    }
+}
+
 int64_t calculate(std::string line) {
     handleBrackets(line);
-    auto operator_pos = line.find_first_of("+*");
-    auto operand1 = std::stoll(line.substr(0, operator_pos));
-    auto next_op_pos = line.find_first_of("+*", operator_pos + 1);
-    auto operand2 = std::stoll(line.substr(operator_pos+1, next_op_pos));
+    handleAdditions(line);
+    auto operator_pos = line.find("*");
     int64_t result;
     if (operator_pos != std::string::npos) {
-        auto op = line[operator_pos];
-        if (op == '+')
-            result = operand1 + operand2;
-        else if(op == '*')
-            result = operand1 * operand2;
-        else
-            assert(false);
+        auto operand1 = std::stoll(line.substr(0, operator_pos));
+        auto next_op_pos = line.find("*", operator_pos + 1);
+        auto operand2 = std::stoll(line.substr(operator_pos+1, next_op_pos));
+        result = operand1 * operand2;
         if (next_op_pos != std::string::npos){
             line.erase(0, next_op_pos);
             line.insert(0, std::to_string(result));
             result = calculate(std::move(line));
         }
     } else {
-        assert(false);
+        result = std::stoll(line);
     }
     return result;
 }
